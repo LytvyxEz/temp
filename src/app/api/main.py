9b22475d0d31d33e356047b0343dc1f23d045ee1
LytvyxEz <maxlytw@gmail.com>
2025-08-14 17:53:00 +0300

    w

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -3,7 +3,3 @@ from app.api.routers import router
 
 app = FastAPI()
 app.include_router(router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.api.main:app", host="127.0.0.1", port=8000, reload=True)
